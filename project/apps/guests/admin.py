bfac0baa528c1d00f903a72c088ada5045e14198
Chris Gilmer <chris.gilmer@gmail.com>
2011-04-18 20:19:56 -0700

    Added Groups and started to iron out the guest list.

--- project/apps/guests/admin.py
+++ project/apps/guests/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
-from guests.models import Guest
+from guests.models import Group,Guest
+
+class GuestInline(admin.TabularInline):
+	model = Guest
 
 class GuestAdmin(admin.ModelAdmin):
-	list_display = ['user','phone','meal','party','position',]
-
+	list_display  = ['group','first_name','last_name',]
+	raw_id_fields = ['group',]
+	search_fields = ['first_name','last_name',]
 admin.site.register(Guest,GuestAdmin)
+
+class GroupAdmin(admin.ModelAdmin):
+	fieldsets = (
+		(None, {
+			'fields': ('name','response','party',)
+			}),
+		('Contact Information', {
+			'fields': ('email','phone','address',
+				'city','state','zipcode',)
+			}),
+		('Admin Use Only', {
+			'fields': ('number_guests',
+				'announcement_required','announcement_sent',
+				'invitation_required','invitation_sent',
+				'thank_you_required','thank_you_sent',
+				'gift_received',)
+			}),
+		)
+	inlines       = [GuestInline,]
+	list_display  = ['name','response','party','email',]
+	search_fields = ['name','gift_received',]
+
+admin.site.register(Group,GroupAdmin)
+
--- project/apps/guests/models.py
+++ project/apps/guests/models.py
@@ -1,78 +1,75 @@
 from django.contrib.auth.models import User
-from django.contrib.localflavor.us.models import USStateField, USPostalCodeField
+from django.contrib.localflavor.us.models import USStateField
 from django.contrib.localflavor.us.us_states import US_STATES
 from django.db import models
 from django.db.models.signals import post_save
 
+class Group(models.Model):
+	""" Guests are invited in groups """
+
+	RESPONSE_CHOICES = (
+		(0,'No Response'),
+		(1,'Attending'),
+		(2,'Not Attending'),
+	)
+	PARTY_CHOICES = (
+		('B','Bride'),
+		('G','Groom'),
+	)
+
+	name     = models.CharField(max_length=80)
+	response = models.IntegerField(choices=RESPONSE_CHOICES, default=0)
+	party    = models.CharField(max_length=1, choices=PARTY_CHOICES, blank=True, null=True)
+	
+	# Contact Information
+	email    = models.EmailField(blank=True, null=True)
+	phone    = models.CharField(max_length=15, blank=True, null=True)
+	address  = models.CharField(max_length=80, blank=True, null=True)
+	city     = models.CharField(max_length=80, blank=True, null=True)
+	state    = USStateField(choices=US_STATES, blank=True, null=True)
+	zipcode  = models.CharField(max_length=10, blank=True, null=True)
+
+	# Admin Only
+	number_guests        = models.PositiveSmallIntegerField(default=1)
+	updated              = models.DateTimeField(auto_now=True)
+
+	announcement_required = models.BooleanField()
+	announcement_sent     = models.DateField(blank=True,null=True)
+	
+	invitation_required  = models.BooleanField()
+	invitation_sent      = models.DateField(blank=True,null=True)
+
+	thank_you_required   = models.BooleanField()
+	thank_you_sent       = models.DateField(blank=True,null=True)
+	gift_received        = models.TextField(blank=True,null=True)
+
+	def __unicode__(self):
+		return self.name
+
+	def save(self,*args,**kwargs):
+		if self.invitation_required:
+			self.announcement_required = True
+		if self.gift_received:
+			self.thank_you_required = True
+		super(Group,self).save(*args,**kwargs)
+
 class Guest(models.Model):
-	""" This keeps information about guests """
+	""" Capture information about each guest """
 
 	MEAL_CHOICES = (
 		('M','Meat'),
 		('F','Fish'),
 		('V','Vegetarian'),
 	)
-	PARTY_CHOICES = (
-		('B','Bride'),
-		('G','Groom'),
-	)
-	POSITION_CHOICES = (
-		('bride','Bride'),
-		('groom','Groom'),
-		('officiant','Officiant'),
-		('best man','Best Man'),
-		('groomsman 1','Groomsman 1'),
-		('groomsman 2','Groomsman 2'),
-		('groomsman 3','Groomsman 3'),
-		('maid of honor','Maid of Honor'),
-		('bridesmaid 1','Bridesmaid 1'),
-		('bridesmaid 2','Bridesmaid 2'),
-		('bridesmaid 3','Bridesmaid 3'),
-		('bride father','Father of the Bride'),
-		('bride mother','Mother of the Bride'),
-		('groom father','Father of the Groom'),
-		('groom mother','Mother of the Groom'),
-		('usher 1','Usher 1'),
-		('usher 2','Usher 2'),
-	)
-	MAIL_CHOICES = (
-		('required','Required'),
-		('written','Written'),
-		('sent','Sent'),
-	)
 
-	user     = models.ForeignKey(User, unique=True)
-	phone    = models.CharField(max_length=15, blank=True, null=True)
-	address  = models.CharField(max_length=80, blank=True, null=True)
-	city     = models.CharField(max_length=80, blank=True, null=True)
-	state    = USStateField(choices=US_STATES)
-	zipcode  = USPostalCodeField()
-	meal     = models.CharField(max_length=1, choices=MEAL_CHOICES, blank=True, null=True)
-	party    = models.CharField(max_length=1, choices=PARTY_CHOICES, blank=True, null=True)
-	message  = models.TextField(blank=True, null=True)
-
-	# Admin use only
-	position = models.CharField(max_length=20, choices=POSITION_CHOICES, unique=True, blank=True, null=True)
-	
-	announcment_required = models.BooleanField()
-	announcment_sent     = models.DateField(blank=True,null=True)
-	
-	invitation_required  = models.BooleanField()
-	invitation_sent      = models.DateField(blank=True,null=True)
-
-	thankyou_required    = models.BooleanField()
-	thankyou_sent        = models.DateField(blank=True,null=True)
-	gift_received        = models.TextField(blank=True,null=True)
-
-	gift_required        = models.BooleanField()
-	gift_sent            = models.DateField(blank=True,null=True)
-	gift_given           = models.TextField(blank=True,null=True)
+	group      = models.ForeignKey(Group)
+	first_name = models.CharField(max_length=30)
+	last_name  = models.CharField(max_length=30)
+	meal       = models.CharField(max_length=1, choices=MEAL_CHOICES, blank=True, null=True)
 
 	def __unicode__(self):
-		return self.user.get_full_name()
-	
-def create_profile(sender, instance=None, **kwargs):
-	if instance is None: return
-	profile, created = Guest.objects.get_or_create(user=instance)
+		return u"%s %s" % (self.first_name,self.last_name)
 
-post_save.connect(create_profile, sender=User)
+	class Meta:
+		ordering = ('last_name','first_name',)
+
