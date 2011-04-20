2c53f4e55c70a802f32108321b03ddc835272914
Chris Gilmer <chris.gilmer@gmail.com>
2011-04-20 08:52:36 -0700

    Added a list display to the wedding admin.
    Started updating templates with the new context.

--- project/apps/weddings/admin.py
+++ project/apps/weddings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from weddings.models import Wedding
 
-class WeddingAdmin(admin.ModelAdmin): pass
+class WeddingAdmin(admin.ModelAdmin):
+	list_display = ['bride','groom','contact_phone','contact_email',
+			'rehearsal_dinner','wedding_ceremony','wedding_reception',]
 
 admin.site.register(Wedding,WeddingAdmin)
