dcdf8251fe76ef7085dbc8f6d94af36ad993318b
Chris Gilmer <chris.gilmer@gmail.com>
2011-04-20 07:34:41 -0700

    Added a datetime field to the messages model.

--- project/apps/messages/models.py
+++ project/apps/messages/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class Message(models.Model):
 
-	text = models.TextField()
-	name = models.CharField(max_length=80)
+	text    = models.TextField()
+	name    = models.CharField(max_length=80)
+	created = models.DateTimeField(auto_now_add=True)
 
 	def __unicode__(self):
 		return self.name
