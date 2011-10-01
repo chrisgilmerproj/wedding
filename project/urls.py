ba3d91493d420c33db4bf56ff5b4d2647e07c704
Chris Gilmer <chris.gilmer@gmail.com>
2011-09-30 22:13:25 -0700

    No longer need special template view

--- project/urls.py
+++ project/urls.py
@@ -5,18 +5,17 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 
-from weddings.views import WeddingView
 
 admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     
-    url(r'^$',          WeddingView.as_view(template_name='homepage.html'), name="home"),
-    url(r'^events/$',   WeddingView.as_view(template_name='events.html'),   name="events"),
-    url(r'^lodging/$',  WeddingView.as_view(template_name='lodging.html'),  name="lodging"),
-    url(r'^story/$',    WeddingView.as_view(template_name='story.html'),    name="story"),
-    url(r'^gallery/$',  WeddingView.as_view(template_name='gallery.html'),  name="gallery"),
+    url(r'^$',          TemplateView.as_view(template_name='homepage.html'), name="home"),
+    url(r'^events/$',   TemplateView.as_view(template_name='events.html'),   name="events"),
+    url(r'^lodging/$',  TemplateView.as_view(template_name='lodging.html'),  name="lodging"),
+    url(r'^story/$',    TemplateView.as_view(template_name='story.html'),    name="story"),
+    url(r'^gallery/$',  TemplateView.as_view(template_name='gallery.html'),  name="gallery"),
 	url(r'^contact/$',  'messages.views.messages', name='contact'),
 	url(r'^rsvp/',      include('rsvp.urls')),
 )
